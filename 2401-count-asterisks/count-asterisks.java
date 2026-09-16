62b8ea3dd0929bd3081b30cbcf4ebae710ea59ac
Palak Gupta <palak.gupta8223@gmail.com>
2026-09-16 22:20:39 +0530

    Time: 3 ms (38.88%) | Memory: 43 MB (34.36%) - LeetSync

--- 2401-count-asterisks/count-asterisks.java
+++ 2401-count-asterisks/count-asterisks.java
@@ -1,13 +1,13 @@
 class Solution {
     public int countAsterisks(String s) {
-        int count =0;
-        boolean inside = false;
+        int count = 0;
+        int barCount = 0;
 
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i) == '|'){
-                inside = !inside;
+                barCount++;
             }
-            else if(s.charAt(i) == '*' && !inside){
+            else if(s.charAt(i) == '*' && barCount % 2 == 0){
                 count++;
             }
         }
