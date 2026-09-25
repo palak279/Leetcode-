a8a5f67e696223c0ba20d3bb48b20fd6e71f4427
Palak Gupta <palak.gupta8223@gmail.com>
2026-09-25 23:10:08 +0530

    Time: 1 ms (84.00%) | Memory: 42.8 MB (55.32%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<Character, String> map1 = new HashMap<>();
+        HashMap<String, Character> map2 = new HashMap<>();
+        String [] words = s.split(" ");
+        char [] arr = pattern.toCharArray();
+        if(arr.length != words.length){
+            return false;
+        }
+        for(int i=0; i<arr.length; i++){
+            if(map1.containsKey(arr[i])){
+                if(!map1.get(arr[i]).equals(words[i])){
+                    return false;
+                }
+            }
+            else{
+                map1.put(arr[i], words[i]);
+            }
+            if(map2.containsKey(words[i])){
+                if(!map2.get(words[i]).equals(arr[i])){
+                    return false;
+                }
+            }
+            else{
+                map2.put(words[i], arr[i]);
+            }
+        }
+        return true;
+    }
+}
